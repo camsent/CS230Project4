36f56a25105b99df6ee2758e84d549f7616adf08
Zuritee <candylichqwq@gmail.com>
2025-11-06 14:33:40 -0500

    added home to app.jsx

--- FrontEnd/frontend-study-app/src/App.jsx
+++ FrontEnd/frontend-study-app/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import RegisterPage from './components/RegisterPage';
+import HomePage from './components/homePage';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </Router>
     </>
