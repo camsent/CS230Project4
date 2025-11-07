f84e6c4be57d2ff9a7100da432569dacda32e190
Zuritee <candylichqwq@gmail.com>
2025-11-07 09:28:51 -0500

    home page improvements

--- FrontEnd/frontend-study-app/src/components/HomePage.jsx
+++ FrontEnd/frontend-study-app/src/components/HomePage.jsx
@@ -5,8 +5,15 @@ import { FaUser, FaLock } from "react-icons/fa"
 const myData = [
   { id: 1, content: "Item 1" },
   { id: 2, content: "Item 2" },
-  { id: 3, content: "Item 3" },
-  { id: 4, content: "Item 4" },
+  { id: 2, content: "Item 2" },
+  { id: 2, content: "Item 2" },
+  { id: 2, content: "Item 2" },
+  { id: 2, content: "Item 2" },
+  { id: 2, content: "Item 2" },
+  { id: 2, content: "Item 2" },
+  { id: 2, content: "Item 2" },
+  { id: 2, content: "Item 2" },
+
 ];
 
 const HomePage = () => {
@@ -15,7 +22,9 @@ const HomePage = () => {
       {myData.map((item) => (
         <div key={item.id} className="stuff">
           {item.content}
-          Stuff
+          <br></br>
+          <button>Delete</button>
+          <button>Edit</button>
         </div>
       ))}
     </div>
