72bfb9ed64ea65f1f32717eca6cbe5ac0e5dcf29
Zuritee <candylichqwq@gmail.com>
2025-11-11 18:30:06 -0500

    fixed merge conflict

--- FrontEnd/frontend-study-app/src/App.jsx
+++ FrontEnd/frontend-study-app/src/App.jsx
@@ -3,11 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import RegisterPage from './components/RegisterPage';
 import HomePage from './components/homePage';
-<<<<<<< HEAD
 import Navbar from './components/NavBar';
-=======
 import StudyPage from './components/StudyPage'
->>>>>>> 4488f3eeb956ebd84aa04141e29fbfb5f6f24c1c
 
 
 function App() {
